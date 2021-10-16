4438ff8a32ae27e78f657f1789504c0a85eaf4e2
dragonnomada <dragonnomada123@gmail.com>
2021-10-16 11:05:09 -0500

    Sesión 1 - Se actualizó el contenido

---
+++ projects/HelloJava/src/main/java/com/mycompany/hellojava/MainApp.java
@@ -0,0 +1,10 @@
+package com.mycompany.hellojava;
+
+public class MainApp {
+    
+    // Método de pruebas
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos al mundo de Java");
+    }
+    
+}
---
+++ projects/HelloJava/src/main/java/com/mycompany/hellojava/OtherApp.java
@@ -0,0 +1,10 @@
+package com.mycompany.hellojava;
+
+public class OtherApp {
+    
+    // Método de pruebas
+    public static void main(String[] args) {
+        System.out.println("Este es un mensaje");
+    }
+    
+}
